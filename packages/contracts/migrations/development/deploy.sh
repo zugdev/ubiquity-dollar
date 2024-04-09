e2263fb8dba78d453e5a60c2d93b22a96eff5691
molecula451 <web3samuraidev@gmail.com>
2024-04-09 16:17:12 -0400

    chore: update

--- packages/contracts/migrations/development/deploy.sh
+++ packages/contracts/migrations/development/deploy.sh
@@ -4,7 +4,7 @@
 source .env
 
 # Deploy001_Diamond_Dollar (deploys Diamond and Dollar contracts)
-forge script migrations/development/Deploy001_Diamond_Dollar.s.sol:Deploy001_Diamond_Dollar --rpc-url $RPC_URL --broadcast -vvvv
+forge script migrations/development/Deploy001_Diamond_Dollar.s.sol:Deploy001_Diamond_Dollar --rpc-url $RPC_URL --broadcast -vvvv --legacy
 
 # Deploy002_Governance (deploys Governance token)
-forge script migrations/development/Deploy002_Governance.s.sol:Deploy002_Governance --rpc-url $RPC_URL --broadcast -vvvv
+forge script migrations/development/Deploy002_Governance.s.sol:Deploy002_Governance --rpc-url $RPC_URL --broadcast -vvvv --legacy
