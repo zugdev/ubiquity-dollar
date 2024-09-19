94f9c4f5733d7119a71678241437cd1b02c8f01c
zugdev <zug.dev@proton.me>
2024-09-19 13:27:33 -0300

    feat: interface natspec

--- packages/contracts/src/dollar/interfaces/IAmo.sol
+++ packages/contracts/src/dollar/interfaces/IAmo.sol
@@ -2,7 +2,15 @@
 pragma solidity ^0.8.19;
 
 interface IAmo {
+    /**
+     * @notice Returns collateral back to the AMO minter
+     * @param collateralAmount Amount of collateral to return
+     */
     function returnCollateralToMinter(uint256 collateralAmount) external;
 
+    /**
+     * @notice Sets the address of the AMO minter
+     * @param _amoMinterAddress New address of the AMO minter
+     */
     function setAmoMinter(address _amoMinterAddress) external;
 }
