5e6a5fc200855df59f75f07dfe70f0ff6d084386
zugdev <zug.dev@proton.me>
2024-09-22 13:16:26 -0300

    feat: remove borrowing from AaveAmo

--- packages/contracts/src/dollar/amo/AaveAmo.sol
+++ packages/contracts/src/dollar/amo/AaveAmo.sol
@@ -7,12 +7,11 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IPool} from "@aavev3-core/contracts/interfaces/IPool.sol";
-import {IPoolDataProvider} from "@aavev3-core/contracts/interfaces/IPoolDataProvider.sol";
 import {IRewardsController} from "@aavev3-periphery/contracts/rewards/interfaces/IRewardsController.sol";
 
 /**
  * @title AaveAmo
- * @notice AMO to interact with Aave V3 and manage rewards and borrowing mechanisms.
+ * @notice AMO to interact with Aave V3: supply and manage rewards.
  * @notice Can receive collateral from UbiquityAmoMinter and interact with Aave's V3 pool.
  */
 contract AaveAmo is IAmo, Ownable {
@@ -24,20 +23,8 @@ contract AaveAmo is IAmo, Ownable {
     /// @notice Aave V3 pool instance
     IPool public immutable aavePool;
 
-    /// @notice Aave token address
-    ERC20 public immutable aaveToken;
-
     /// @notice Aave rewards controller
     IRewardsController public immutable aaveRewardsController;
-
-    /// @notice Aave data provider
-    IPoolDataProvider public immutable aavePoolDataProvider;
-
-    /// @notice List of borrowed assets from Aave
-    address[] public aaveBorrowedAssets;
-
-    /// @notice Mapping for tracking borrowed assets
-    mapping(address => bool) public aaveIsAssetBorrowed;
 
     /* ========== CONSTRUCTOR ========== */
 
@@ -46,17 +33,13 @@ contract AaveAmo is IAmo, Ownable {
      * @param _ownerAddress Address of the contract owner
      * @param _amoMinterAddress Address of the Ubiquity Amo minter
      * @param _aavePool Address of the Aave pool
-     * @param _aaveToken Address of the Aave token
      * @param _aaveRewardsController Address of the Aave rewards controller
-     * @param _aavePoolDataProvider Address of the Aave data provider
      */
     constructor(
         address _ownerAddress,
         address _amoMinterAddress,
         address _aavePool,
-        address _aaveToken,
-        address _aaveRewardsController,
-        address _aavePoolDataProvider
+        address _aaveRewardsController
     ) {
         require(_ownerAddress != address(0), "Owner address cannot be zero");
         require(
@@ -64,14 +47,9 @@ contract AaveAmo is IAmo, Ownable {
             "Amo minter address cannot be zero"
         );
         require(_aavePool != address(0), "Aave pool address cannot be zero");
-        require(_aaveToken != address(0), "Aave address cannot be zero");
         require(
             _aaveRewardsController != address(0),
             "Aave rewards controller address cannot be zero"
-        );
-        require(
-            _aavePoolDataProvider != address(0),
-            "Aave pool data provider address cannot be zero"
         );
 
         // Set contract owner
@@ -83,14 +61,8 @@ contract AaveAmo is IAmo, Ownable {
         // Set the Aave pool
         aavePool = IPool(_aavePool);
 
-        // Set the Aave token
-        aaveToken = ERC20(_aaveToken);
-
         // Set the Aave rewards controller
         aaveRewardsController = IRewardsController(_aaveRewardsController);
-
-        // Set the Aave pool data provider
-        aavePoolDataProvider = IPoolDataProvider(_aavePoolDataProvider);
     }
 
     /* ========== Aave V3 + REWARDS ========== */
@@ -126,48 +98,6 @@ contract AaveAmo is IAmo, Ownable {
     }
 
     /**
-     * @notice Borrows an asset from Aave pool
-     * @param asset Address of the asset to borrow
-     * @param borrowAmount Amount of asset to borrow
-     * @param interestRateMode Interest rate mode: 1 for Stable, 2 for Variable
-     */
-    function aaveBorrow(
-        address asset,
-        uint256 borrowAmount,
-        uint256 interestRateMode
-    ) public onlyOwner {
-        aavePool.borrow(
-            asset,
-            borrowAmount,
-            interestRateMode,
-            0,
-            address(this)
-        );
-        aaveIsAssetBorrowed[asset] = true;
-        aaveBorrowedAssets.push(asset);
-
-        emit Borrowed(asset, borrowAmount, interestRateMode);
-    }
-
-    /**
-     * @notice Repays a borrowed asset to Aave pool
-     * @param asset Address of the asset to repay
-     * @param repayAmount Amount of asset to repay
-     * @param interestRateMode Interest rate mode: 1 for Stable, 2 for Variable
-     */
-    function aaveRepay(
-        address asset,
-        uint256 repayAmount,
-        uint256 interestRateMode
-    ) public onlyOwner {
-        ERC20 token = ERC20(asset);
-        token.safeApprove(address(aavePool), repayAmount);
-        aavePool.repay(asset, repayAmount, interestRateMode, address(this));
-
-        emit Repaid(asset, repayAmount, interestRateMode);
-    }
-
-    /**
      * @notice Claims all rewards available from the list of assets provided, will fail if balance on asset is zero
      * @param assets Array of aTokens/sTokens/vTokens addresses to claim rewards from
      */
@@ -182,7 +112,7 @@ contract AaveAmo is IAmo, Ownable {
 
     /**
      * @notice Returns collateral back to the AMO minter
-     * @param collateralAmount Amount of collateral to return
+     * @param collateralAmount Amount of collateral to return, pass 0 to return all collateral
      */
     function returnCollateralToMinter(
         uint256 collateralAmount
@@ -253,16 +183,6 @@ contract AaveAmo is IAmo, Ownable {
     event CollateralWithdrawn(
         address indexed collateralAddress,
         uint256 amount
-    );
-    event Borrowed(
-        address indexed asset,
-        uint256 amount,
-        uint256 interestRateMode
-    );
-    event Repaid(
-        address indexed asset,
-        uint256 amount,
-        uint256 interestRateMode
     );
     event CollateralReturnedToMinter(uint256 amount);
     event RewardsClaimed();
