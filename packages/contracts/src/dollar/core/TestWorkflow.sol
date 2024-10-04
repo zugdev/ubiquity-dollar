09bb1bfbd26432c430fd4f9056a7e62aeaa16a77
zugdev <zug.dev@proton.me>
2024-10-03 21:26:15 -0300

    feat: add random comments to test storage check workflow

--- packages/contracts/src/dollar/core/TestWorkflow.sol
+++ packages/contracts/src/dollar/core/TestWorkflow.sol
@@ -2,9 +2,11 @@
 pragma solidity 0.8.19;
 
 contract TestWorkflow {
+    // random comment
     address public owner;
     uint256 public value3;
     uint256 public value2;
     uint256 public value;
+    // random comment
     uint256 public value4;
 }
